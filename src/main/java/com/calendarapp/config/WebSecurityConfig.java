549f985009e91dba9c0b3977a806fae9eddda529
Daniel Wolski <danielwolski1532@gmail.com>
2024-12-19 10:52:53 +0100

    Add Spring Security 2

--- src/main/java/com/calendarapp/config/WebSecurityConfig.java
+++ src/main/java/com/calendarapp/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.calendarapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,12 +18,14 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+			.cors(Customizer.withDefaults())
 			.authorizeHttpRequests((requests) -> requests
 				.requestMatchers("/", "/home").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
 				.loginPage("/login")
+				.defaultSuccessUrl("http://localhost:4200", true)
 				.permitAll()
 			)
 			.logout((logout) -> logout.permitAll());
